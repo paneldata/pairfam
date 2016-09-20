c667f08a4325d6110867ff5d63c03c56887fce1b
mhebing <marcel@ddionrails.org>
2016-09-20 15:28:17 +0200

    Update pairfam to datasets.json

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 sys.path.append(os.path.expanduser("~/github/ddi.py/"))
 
-from ddi.onrails.repos import merge_instruments, dor1, copy, extract_variables
+from ddi.onrails.repos import merge_instruments, dor1, copy, extract_variables, convert_r2ddi
 
 def main():
     copy.study()
-    copy.r2ddi("r2ddi/v7/en", "ddionrails/r2ddi/v7")
     extract_variables.XmlParser("r2ddi/v7/en", "pairfam").run()
+    convert_r2ddi.Parser(version="v7").write_json()
 
 if __name__ == "__main__":
     main()
