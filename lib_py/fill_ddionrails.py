c7baade087a45e2468a5d7deea094502b357569f
mhebing <marcel@ddionrails.org>
2016-09-23 09:33:33 +0200

    Fill ddionrails

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -6,9 +6,10 @@ sys.path.append(os.path.expanduser("~/github/ddi.py/"))
 from ddi.onrails.repos import merge_instruments, dor1, copy, extract_variables, convert_r2ddi
 
 def main():
-    copy.study()
+    copy.f("study.md")
     extract_variables.XmlParser("r2ddi/v7/en", "pairfam").run()
     convert_r2ddi.Parser(version="v7").write_json()
+    copy.f("datasets.csv")
 
 if __name__ == "__main__":
     main()
